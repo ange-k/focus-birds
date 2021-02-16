53b4cbe1691619f38539e6e2f79977b633d83483
shogo takayama <shogo@chalkboard.me>
2021-02-16 23:19:20 +0900

    gradle最適化

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 
 plugins {
   id("org.springframework.boot") version "2.4.2" apply false
-  id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-  kotlin("jvm") version "1.4.21" apply false
-  kotlin("plugin.spring") version "1.4.21" apply false
-  kotlin("kapt") version "1.3.61" apply false
+  id("org.jetbrains.kotlin.jvm") version "1.4.21"
+  id("org.jetbrains.kotlin.plugin.spring") version "1.4.21"
+  id("org.jetbrains.kotlin.kapt") version "1.4.21"
 }
 
 allprojects {
@@ -18,9 +16,9 @@ allprojects {
 }
 
 subprojects {
-  apply(plugin = "io.spring.dependency-management")
   apply(plugin = "kotlin")
-  apply(plugin = "kotlin-spring")
+  apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+  apply(plugin = "io.spring.dependency-management")
   apply(plugin = "kotlin-kapt")
 
   the<DependencyManagementExtension>().apply {
@@ -29,36 +27,40 @@ subprojects {
     }
   }
 
-  tasks.withType<Jar> {
-    enabled = true
-  }
-
-  tasks.withType<KotlinCompile> {
-    kotlinOptions {
-      freeCompilerArgs = listOf("-Xjsr305=strict")
-      jvmTarget = "11"
+  tasks {
+    compileKotlin {
+      kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+      }
     }
-  }
-  tasks.withType<Test> {
-    useJUnitPlatform()
+    compileTestKotlin {
+      kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+      }
+    }
+    test {
+      useJUnitPlatform()
+    }
   }
 
   dependencies {
-    "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin")
-    "implementation"("org.jetbrains.kotlin:kotlin-reflect")
-    "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // other
-    "compileOnly"("org.projectlombok:lombok")
-    "annotationProcessor"("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
     // testing
-    "testImplementation"("org.springframework.boot:spring-boot-starter-test") {
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
       exclude("org.junit.vintage", "junit-vintage-engine")
     }
-    "testImplementation"("org.springframework.boot:spring-boot-starter-test")
-    "testImplementation"("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.assertj:assertj-core:3.11.1")
 
-    "kapt"("org.springframework.boot:spring-boot-configuration-processor")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
   }
 }
--- focus-birds-core/build.gradle.kts
+++ focus-birds-core/build.gradle.kts
@@ -1,14 +1,11 @@
-plugins {
-	application
+apply(plugin ="org.springframework.boot")
+
+tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
+	mainClass.set("chalkboard.me.focusbirdscore.FocusBirdsCoreApplicationKt")
 }
 
 dependencies {
 	implementation(project(":twitter_api_client"))
 	implementation("org.springframework.boot:spring-boot-starter-batch")
 	testImplementation("org.springframework.batch:spring-batch-test")
-}
-
-// https://docs.gradle.org/current/userguide/application_plugin.html
-application {
-	mainClass.set("chalkboard.me.focusbirdscore.FocusBirdsCoreApplication")
 }
